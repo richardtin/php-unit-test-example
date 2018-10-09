362235da8d8551d9d67b51d3f9cf1bdd4dee6344
Richard Ting <richard.tin@gmail.com>
2018-10-09 17:09:22 +0800

    test(otp): use mockery instead of manual stub class

--- tests/otp/AuthenticationServiceTest.php
+++ tests/otp/AuthenticationServiceTest.php
@@ -5,33 +5,48 @@ namespace Tests\otp;
 use App\otp\AuthenticationService;
 use App\otp\IProfile;
 use App\otp\IToken;
+use Mockery as m;
 use PHPUnit\Framework\TestCase;
 
 class AuthenticationServiceTest extends TestCase
 {
+    private $stubProfile;
+
+    private $stubToken;
+
+    private $target;
+
+    protected function setUp()
+    {
+        $this->stubProfile = m::mock(IProfile::class);
+        $this->stubToken = m::mock(IToken::class);
+        $this->target = new AuthenticationService($this->stubProfile, $this->stubToken);
+    }
+
     public function testIsValid()
     {
-        $target = new AuthenticationService(new StubProfile(), new StubToken());
-        $actual = $target->isValid('richard', '91000000');
+        $this->givenProfile('richard', '91');
+        $this->givenToken('000000');
+
+        $this->shouldBeValid('richard', '91000000');
+    }
+
+    private function givenProfile($account, $password): void
+    {
+        $this->stubProfile->shouldReceive('getPassword')
+            ->with($account)
+            ->andReturn($password);
+    }
+
+    private function givenToken($token): void
+    {
+        $this->stubToken->shouldReceive('getRandom')
+            ->andReturn($token);
+    }
+
+    private function shouldBeValid($account, $password): void
+    {
+        $actual = $this->target->isValid($account, $password);
         $this->assertTrue($actual);
-    }
-}
-
-class StubProfile implements IProfile
-{
-    public function getPassword($account)
-    {
-        if ($account === 'richard') {
-            return '91';
-        }
-        return '';
-    }
-}
-
-class StubToken implements IToken
-{
-    public function getRandom($account)
-    {
-        return '000000';
     }
 }
